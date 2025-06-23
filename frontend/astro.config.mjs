32032715fa920b948b5ad11af5df8a24d3edac8f
riyanhossain <mehraabriyan@gmail.com>
2025-06-23 16:01:59 +0600

    update

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -3,9 +3,16 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import vue from '@astrojs/vue';
+
+// import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
+  // adapter: cloudflare()
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [vue()]
 });
