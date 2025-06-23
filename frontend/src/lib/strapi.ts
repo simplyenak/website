c4da1bdca6de1cb9b4bee24ef3f5b8782ede2546
Saymon Arefin Akash <saymon.akash@lifted.dev>
2025-06-23 23:51:14 +0600

    basic setup

--- frontend/src/lib/strapi.ts
+++ frontend/src/lib/strapi.ts
@@ -1,8 +1,8 @@
 interface Props {
-  endpoint: string;
-  query?: Record<string, string>;
-  wrappedByKey?: string;
-  wrappedByList?: boolean;
+  endpoint: string
+  query?: Record<string, string>
+  wrappedByKey?: string
+  wrappedByList?: boolean
 }
 
 /**
@@ -19,27 +19,30 @@ export default async function fetchApi<T>({
   wrappedByKey,
   wrappedByList,
 }: Props): Promise<T> {
-  if (endpoint.startsWith("/")) {
-    endpoint = endpoint.slice(1);
+  if (endpoint.startsWith('/')) {
+    endpoint = endpoint.slice(1)
   }
 
-  const url = new URL(`${import.meta.env.STRAPI_URL}/api/${endpoint}`);
+  const strapiUrl =
+    import.meta.env.STRAPI_URL || 'https://api.system.simplyenak.com'
+  const baseUrl = strapiUrl.endsWith('/') ? strapiUrl.slice(0, -1) : strapiUrl
+  const url = new URL(`${baseUrl}/api/${endpoint}`)
 
   if (query) {
     Object.entries(query).forEach(([key, value]) => {
-      url.searchParams.append(key, value);
-    });
+      url.searchParams.append(key, value)
+    })
   }
-  const res = await fetch(url.toString());
-  let data = await res.json();
+  const res = await fetch(url.toString())
+  let data = await res.json()
 
   if (wrappedByKey) {
-    data = data[wrappedByKey];
+    data = data[wrappedByKey]
   }
 
   if (wrappedByList) {
-    data = data[0];
+    data = data[0]
   }
 
-  return data as T;
+  return data as T
 }
