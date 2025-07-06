36a7efc060838e975debe0b0886d8053d608a2e7
Saymon Arefin Akash <saymon.akash@lifted.dev>
2025-07-06 18:57:46 +0600

    Footer

--- backend/types/generated/components.d.ts
+++ backend/types/generated/components.d.ts
@@ -12,10 +12,39 @@ export interface NavItemsItem extends Struct.ComponentSchema {
   };
 }
 
+export interface PartnerPartner extends Struct.ComponentSchema {
+  collectionName: 'components_partner_partners';
+  info: {
+    displayName: 'partner';
+  };
+  attributes: {
+    image: Schema.Attribute.Media<'images'> & Schema.Attribute.Required;
+    name: Schema.Attribute.String &
+      Schema.Attribute.Private &
+      Schema.Attribute.DefaultTo<'partner'>;
+    url: Schema.Attribute.String;
+  };
+}
+
+export interface PaymentMethodPaymentMethod extends Struct.ComponentSchema {
+  collectionName: 'components_payment_method_payment_methods';
+  info: {
+    displayName: 'paymentMethods';
+  };
+  attributes: {
+    images: Schema.Attribute.Media<'images', true> & Schema.Attribute.Required;
+    name: Schema.Attribute.String &
+      Schema.Attribute.Private &
+      Schema.Attribute.DefaultTo<'Payment Methods Images'>;
+  };
+}
+
 declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
       'nav-items.item': NavItemsItem;
+      'partner.partner': PartnerPartner;
+      'payment-method.payment-method': PaymentMethodPaymentMethod;
     }
   }
 }
