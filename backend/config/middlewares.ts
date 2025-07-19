2b67856579e2d130b14fd1845cafd3d44452817a
riyanhossain <mehraabriyan@gmail.com>
2025-07-20 01:15:41 +0600

    fix: thumbnail

--- backend/config/middlewares.ts
+++ backend/config/middlewares.ts
@@ -1,7 +1,32 @@
 export default [
   "strapi::logger",
   "strapi::errors",
-  "strapi::security",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "market-assets.strapi.io",
+            "se-website-images.s3.nl-ams.scw.cloud",
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "market-assets.strapi.io",
+            "se-website-images.s3.nl-ams.scw.cloud",
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   "strapi::cors",
   "strapi::poweredBy",
   "strapi::query",
