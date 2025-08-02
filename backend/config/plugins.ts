0d9d64eae04b63762aefb73bdff88f21f652bc4c
riyanhossain <mehraabriyan@gmail.com>
2025-08-02 13:54:51 +0600

    fix: s3 plugin

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -4,14 +4,16 @@ module.exports = ({ env }) => ({
     config: {
       provider: "aws-s3",
       providerOptions: {
-        credentials: {
-          accessKeyId: env("SCALEWAY_ACCESS_KEY_ID"),
-          secretAccessKey: env("SCALEWAY_ACCESS_SECRET"),
-        },
-        region: env("SCALEWAY_REGION"), // e.g "fr-par"
-        endpoint: env("SCALEWAY_ENDPOINT"), // e.g. "https://s3.fr-par.scw.cloud"
-        params: {
-          Bucket: env("SCALEWAY_BUCKET"),
+        s3Options: {
+          credentials: {
+            accessKeyId: env("SCALEWAY_ACCESS_KEY_ID"),
+            secretAccessKey: env("SCALEWAY_ACCESS_SECRET"),
+          },
+          region: env("SCALEWAY_REGION"), // e.g "fr-par"
+          endpoint: env("SCALEWAY_ENDPOINT"), // e.g. "https://s3.fr-par.scw.cloud"
+          params: {
+            Bucket: env("SCALEWAY_BUCKET"),
+          },
         },
       },
     },
