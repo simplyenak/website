540cfd4374151d2dd836bbeb821bd83da199184d
SAYMON AREFIN AKASH <simonakash310@gmail.com>
2025-08-25 02:43:01 +0600

    update

--- backend/config/middlewares.ts
+++ backend/config/middlewares.ts
@@ -30,7 +30,19 @@ export default [
   "strapi::cors",
   "strapi::poweredBy",
   "strapi::query",
-  "strapi::body",
+  {
+    name: "strapi::body",
+    config: {
+      formLimit: "500mb",
+      jsonLimit: "500mb",
+      textLimit: "500mb",
+      formidable: {
+        maxFileSize: 500 * 1024 * 1024,
+        maxFields: 1000,
+        maxFieldsSize: 500 * 1024 * 1024,
+      },
+    },
+  },
   "strapi::session",
   "strapi::favicon",
   "strapi::public",
