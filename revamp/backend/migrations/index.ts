9486bed3955bd734f0efa4293aaf23a14755f772
Maarten <maarten@simplyenak.com>
2026-08-05 18:35:31 +0800

    revert: remove stories_locations_ref migration (broken stories API)

--- revamp/backend/migrations/index.ts
+++ revamp/backend/migrations/index.ts
@@ -1,6 +1,5 @@
 import * as migration_20260805_074548_20260805_baseline from './20260805_074548_20260805_baseline';
 import * as migration_20260805_100000_content_briefs_status_enums from './20260805_100000_content_briefs_status_enums';
-import * as migration_20260805_175024_stories_locations_ref from './20260805_175024_stories_locations_ref';
 
 export const migrations = [
   {
@@ -12,10 +11,5 @@ export const migrations = [
     up: migration_20260805_100000_content_briefs_status_enums.up,
     down: migration_20260805_100000_content_briefs_status_enums.down,
     name: '20260805_100000_content_briefs_status_enums'
-  },
-  {
-    up: migration_20260805_175024_stories_locations_ref.up,
-    down: migration_20260805_175024_stories_locations_ref.down,
-    name: '20260805_175024_stories_locations_ref'
   },
 ];
